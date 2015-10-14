b02865fa5d090c426a51521b4b8f9ea5aab7c45a
Tino Sering <konstantin.sering@gmail.com>
2015-10-14 12:21:47 +0200

    adds tests and example corpus and events file

--- tests/test_counting.py
+++ tests/test_counting.py
@@ -1,10 +1,22 @@
+#!/usr/bin/env python3
+# run py.test-3 from the above folder
+
 from .. import counting
 
 def test_cues_outcomes():
-    cues, outcomes = counting.cues_outcomes("test_events.tab")
-    cues3, outcomes3 = counting.cues_outcomes("test_events.tab",
+    cues, outcomes = counting.cues_outcomes("./tests/events.tab")
+    cues3, outcomes3 = counting.cues_outcomes("./tests/events.tab",
                                               number_of_processes=3,
                                               verbose=False)
     assert cues == cues3
     assert outcomes == outcomes3
+
+
+def test_words_symbols():
+    words, symbols = counting.words_symbols("./tests/corpus.txt")
+    words3, symbols3 = counting.words_symbols("./tests/corpus.txt",
+                                              number_of_processes=3,
+                                              verbose=False)
+    assert words == words3
+    assert symbols == symbols3
 
