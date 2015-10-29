14f58363d99390154a49b8b1f0cf5455a04b8692
Tino Sering <konstantin.sering@gmail.com>
2015-10-29 12:10:38 +0100

    adds experimental code to kalman.py

--- kalman.py
+++ kalman.py
@@ -16,6 +16,7 @@ developed on the basis of a matrix algebra solution.
 
 """
 
+
 def update_covariance_matrix(covmat, cues_present_vec, *, noise_parameter=1):
     """
     Update the covariance matrix ``covmat`` in place (!!).
@@ -34,12 +35,34 @@ def update_covariance_matrix(covmat, cues_present_vec, *, noise_parameter=1):
     #if not isinstance(cues_present_vec, np.matrix):
     #    raise ValueError("cues_present_vec need to be a np.matrix.")
 
-    numerator = covmat * cues_present_vec * cues_present_vec.T * covmat
-    denominator = float(noise_parameter + cues_present_vec.T * covmat *
-                        cues_present_vec)
+    denominator = float(noise_parameter + cues_present_vec.T * (covmat *
+                        cues_present_vec))
     # inplace manipulation (bad behaviour, but memory efficient)
-    covmat -= numerator / denominator
-    return covmat
+    covmat -= (covmat * cues_present_vec * cues_present_vec.T * covmat) / denominator
+
+
+def update_covariance_loop(covmat, cues_present, cue_index_map, *, noise_parameter=1):
+    """
+    Update the covariance matrix ``covmat`` in place (!!).
+
+    Parameters
+    ==========
+
+    covmat : covariance matrix of the cues (square matrix)
+    cues_present : list of cues that are present
+    cue_index_map : dict which maps the index to the cues
+    noise_parameter : modulates the learning rate
+
+    """
+    idxs = [cue_index_map[cue] for cue in cues_present]
+    denominator = float(noise_parameter + sum(covmat[idx, idx] for idx in idxs))
+    # assuming the matrix is symmetric
+    cue_cov_sum = np.sum(covmat[:,idxs], axis=1)
+    for nn, value in enumerate(cue_cov_sum):
+        if value == 0:
+            continue
+        covmat[:, nn] -= float(value) * cue_cov_sum / denominator
+
 
 
 def update_mu_matrix(mumat, covmat, cues_present_vec, outcomes_present_vec, *,
@@ -75,7 +98,6 @@ def update_mu_matrix(mumat, covmat, cues_present_vec, outcomes_present_vec, *,
     # not memory efficient in this case
 
     mumat += (covmat * (cues_present_vec * (outcomes_present_vec.T - cues_present_vec.T * mumat))) / denominator
-    return mumat
 
 
 
@@ -94,23 +116,23 @@ def main():
     # building a cue->index map
     tmp = list(cues.keys())
     tmp.sort()
-    cues = dict(zip(tmp, range(len(tmp))))
+    cue_index_map = dict(zip(tmp, range(len(tmp))))
     del tmp
     # building a outcome->index map
     tmp = list(outcomes.keys())
     tmp.sort()
-    outcomes = dict(zip(tmp, range(len(tmp))))
+    outcome_index_map = dict(zip(tmp, range(len(tmp))))
     del tmp
 
     # mumat has cues as rows and outcomes as columns
-    mumat = np.matrix(np.zeros((len(cues), len(outcomes)), dtype=np.double))
+    mumat = np.matrix(np.zeros((len(cue_index_map), len(outcome_index_map)), dtype=np.double))
     print("mumat: " + str(mumat.shape))
 
     # covmat has cues as rows and columns
-    covmat = np.matrix(np.diag(np.ones(len(cues), dtype=np.double)))
+    covmat = np.matrix(np.diag(np.ones(len(cue_index_map), dtype=np.double)))
 
-    cue_vector = np.matrix(np.zeros((len(cues), 1)), dtype=np.double)
-    outcome_vector = np.matrix(np.zeros((len(outcomes), 1)), dtype=np.double)
+    cue_vector = np.matrix(np.zeros((len(cue_index_map), 1)), dtype=np.double)
+    outcome_vector = np.matrix(np.zeros((len(outcome_index_map), 1)), dtype=np.double)
 
 
     # generating sparse column vectors out of event
@@ -125,21 +147,26 @@ def main():
             freq = int(freq)
 
             for cue in cues_line:
-                cue_vector[cues[cue], 0] = 1.0
+                cue_vector[cue_index_map[cue], 0] = 1.0
             for outcome in outcomes_line:
-                outcome_vector[outcomes[outcome], 0] = 1.0
+                outcome_vector[outcome_index_map[outcome], 0] = 1.0
 
             # update mumat and covmat inplace
             # first update mumat as it depends on covmat
             for ii in range(freq):
-                mumat = update_mu_matrix(mumat, covmat, cue_vector, outcome_vector)
-                covmat = update_covariance_matrix(covmat, cue_vector)
+                # apply dynamic change
+                #mumat = D * mumat  # D is identity
+                #covmat = D * covmat * D.T + U  # D is identity
+
+                # update with next event
+                update_mu_matrix(mumat, covmat, cue_vector, outcome_vector)
+                update_covariance_matrix(covmat, cue_vector)
 
             # set values back to zero!!!
             for cue in cues_line:
-                cue_vector[cues[cue]] = 0.0
+                cue_vector[cue_index_map[cue]] = 0.0
             for outcome in outcomes_line:
-                outcome_vector[outcomes[outcome]] = 0.0
+                outcome_vector[outcome_index_map[outcome]] = 0.0
             print(".", end="")
             sys.stdout.flush()
 
@@ -147,10 +174,10 @@ def main():
     np.savetxt("mumat.txt", mumat)
     np.savetxt("covmat.txt", covmat)
 
-    #plt.imshow(mumat)
-    #plt.show()
-    #plt.imshow(covmat)
-    #plt.show()
+    plt.imshow(mumat)
+    plt.show()
+    plt.imshow(covmat)
+    plt.show()
 
 
 if __name__=="__main__":
