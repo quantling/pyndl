5accfecb4254157fa4a9ed1233fcc3b7db498bfc
Tino Sering <konstantin.sering@gmail.com>
2015-10-29 12:11:24 +0100

    improves preprocessing; adds code to generate binary event files

--- preprocessing.py
+++ preprocessing.py
@@ -121,33 +121,6 @@ def create_eventfile(corpus_file,
                 raise NotImplementedError("This combination of context=%s and event=%s is not implemented yet." % (str(context), str(event)))
 
 
-
-import sys
-def attach_one(filename):
-    with open(filename + ".withone", "wt") as out_file:
-        out_file.write("cues\toutcomes\tfrequency\n")
-        with open(filename, "rt") as in_file:
-            for ii, line in enumerate(in_file):
-                out_file.write(line.strip() + "\t1\n")
-                if ii % 100000 == 0:
-                    print(".", end="")
-                    sys.stdout.flush()
-
-if __name__ == "__main__":
-    #attach_one("ro.subtitles.events.utf8")
-
-    #corpus_file = "/home/tino/collab-petar/pl.subtitles.utf8"
-    #event_file = "/home/tino/collab-petar/pl.subtitles.events.utf8"
-    corpus_file = "./tests/corpus.txt"
-    event_file = "./tests/events_corpus.tab"
-    symbols = "abcdefghijklmnopqrstuvwxyzóąćęłńśźż"  # polish
-    #symbols = "aâăbcdefghiîjklmnopqrsştţuvwxyz")  # romanian
-
-    create_eventfile(corpus_file, event_file, symbols,
-                     context="document", event="consecutive_words",
-                     event_option=3, lower_case=True, verbose=True)
-
-
 ################
 ## Preprocessing
 ################
@@ -160,7 +133,7 @@ def to_bytes(int_):
     return int_.to_bytes(4, 'little')
 
 
-def write_events(filename, events):
+def write_events(filename, events, *, start=0, stop=4294967296):
     """
     Write out a list of events to a disk file in binary format.
 
@@ -169,10 +142,10 @@ def write_events(filename, events):
 
     Parameters
     ----------
-
     filename : string
-
     events : iterator of (cue_ids, outcome_ids, frequency) triples called event
+    start : first event to write (zero based index)
+    stop : last event to write (zero based index; excluded)
 
     Binary Format
     -------------
@@ -189,6 +162,10 @@ def write_events(filename, events):
         nr of cues in second event
         ...
 
+    Raises
+    ------
+    StopIteration : events generator is exhausted before stop is reached
+
     """
 
     with open(filename, "wb") as out_file:
@@ -197,8 +174,19 @@ def write_events(filename, events):
         out_file.write(to_bytes(CURRENT_VERSION))
 
         # events
-        out_file.write(to_bytes(len(events)))
-        for event in events:
+        # estimated number of events (will be rewritten if the actual number
+        # differs)
+        n_events_estimate = stop - start
+        out_file.write(to_bytes(n_events_estimate))
+
+        n_events = 0
+        for ii, event in enumerate(events):
+            if ii < start:
+                continue
+            if ii >= stop:
+                break
+
+            n_events += 1
             cue_ids, outcome_ids, frequency = event
 
             # cues in event
@@ -213,6 +201,15 @@ def write_events(filename, events):
 
             # frequency
             out_file.write(to_bytes(frequency))
+
+        if n_events != n_events_estimate:
+            # the generator was exhausted earlier
+            out_file.seek(8)
+            out_file.write(to_bytes(n_events))
+            raise StopIteration("event generator was exhausted before stop")
+
+
+
 
 def event_generator(corpus_file, cue_id_map, outcome_id_map, *, sort_within_event=False):
     with open(corpus_file, "rt") as in_file:
@@ -237,37 +234,84 @@ def event_generator(corpus_file, cue_id_map, outcome_id_map, *, sort_within_even
             yield event
 
 
-
-
-def process_tabular_corpus(corpus_file, cue_id_map, outcome_id_map, path_name,
-                           *, sort_within_event=False, n_threads=2):
+def create_binary_event_files(path_name, event_file, cue_id_map,
+                              outcome_id_map,
+                              *, sort_within_event=False, number_of_processes=2,
+                              events_per_file=1000000, overwrite=False,
+                              verbose=False):
     """
-    Creates the binary event file format for a tabular cue outcome frequency corpus.
+    Creates the binary event files for a tabular cue outcome frequency corpus.
 
     Parameters
     ----------
-    corpus_file : string
+    path_name : str
+        folder name where to store the binary event files
+    event_file : str
         path to tab separated text file that contains all events in a cue
         outcome frequency table.
     cue_id_map : dict (str -> int)
         cue to id map
     outcome_id_map : dict (str -> int)
         outcome to id map
-    path_name : str
-        folder name where to store the binary event files
     sort_within_event : bool
         should we sort the cues and outcomes within the event
-    n_threads : int
+    number_of_processes : int
         number of threads to use
+    events_per_file : int
+    overwrite : overwrite files if they exist
+    verbose : bool
 
     """
-    event_file = os.path.join(path_name, "events_0_1.dat")
 
-    raise NotImplementedError()
-    #with open(event_file, "wb") as out_file:
+    if not os.path.isdir(path_name):
+        if verbose:
+            print("create event file folder '%s'" % path_name)
+        os.mkdir(path_name, 0o773)
+    elif not overwrite:
+        raise IOError("folder %s exists and overwrite is False" % path_name)
+    else:
+        if verbose:
+            print("removing event files in '%s'" % path_name)
+        for file_name in os.listdir(path_name):
+            if "events_0_" in file_name:
+                os.remove(os.path.join(path_name, file_name))
 
+    with multiprocessing.Pool(number_of_processes) as pool:
 
+        def error_callback(error):
+            if isinstance(error, StopIteration):
+                pool.close()
+            else:
+                raise error
 
+        ii = 0
+        while True:
+            kwargs = {"filename": os.path.join(path_name, "events_0_%i.dat" % ii),
+                      "events": event_generator(event_file,
+                                                cue_id_map,
+                                                outcome_id_map,
+                                                sort_within_event=sort_within_event),
+                      "start": ii*events_per_file,
+                      "stop": (ii+1)*events_per_file}
+            try:
+                pool.apply_async(write_events, kwds=kwargs,
+                                 error_callback=error_callback)
+                if verbose:
+                    print("submitted job %i" % ii)
+            except ValueError as error:
+                # someone has closed the pool with the correct error callback
+                if error.args[0] == 'Pool not running':
+                    print("reached end of events")
+                    break  # out of while True
+                else:
+                    raise error
+            ii += 1
+            # only start jobs in chunks of 4*number_of_processes
+            if ii % (number_of_processes*4) == 0:
+                pool.wait()
+        # wait until all jobs are done
+        pool.wait()
+        print("finished all jobs.\n")
 
 
 def main():
@@ -277,5 +321,36 @@ def main():
     outcomes = read_outcomes(outcome_file)
     outcomes.sort()
     outcome_id_map = {outcome: nn for nn, outcome in enumerate(outcomes)}
+
+
+if __name__ == "__main__":
+
+    from counting import cues_outcomes
+
+    corpus_file = "./tests/corpus.txt"
+    event_file = "./tests/events_corpus.tab"
+    symbols = "abcdefghijklmnopqrstuvwxyzóąćęłńśźż"  # polish
+
+    #symbols = "aâăbcdefghiîjklmnopqrsştţuvwxyz")  # romanian
+
+    create_eventfile(corpus_file, event_file, symbols,
+                     context="document", event="consecutive_words",
+                     event_option=3, lower_case=True, verbose=True)
+
+    cue_freq_map, outcome_freq_map = cues_outcomes(event_file,
+                                                   number_of_processes=2)
+    cues = cue_freq_map.keys()
+    cues.sort()
+    cue_id_map = {cue: ii for ii, cue in enumerate(cues)}
+
+    outcomes = outcome_freq_map.keys()
+    outcomes.sort()
+    outcome_id_map = {outcome: nn for nn, outcome in enumerate(outcomes)}
+
+    path_name = event_file + ".events"
+    create_binary_event_files(path_name, event_file, cue_id_map,
+                              outcome_id_map, sort_within_event=False,
+                              number_of_processes=2, events_per_file=100, overwrite=True,
+                              verbose=True)
 
 
