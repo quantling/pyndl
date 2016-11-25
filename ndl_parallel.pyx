1792941a697c2cdfabe7e4497ba41eb66412f144
Tino Sering <konstantin.sering@uni-tuebingen.de>
2016-11-25 15:21:10 +0100

    fixes nogil inline code; compiles but gives wrong results half the time wring results

--- ndl_parallel.pyx
+++ ndl_parallel.pyx
@@ -5,9 +5,15 @@ from cython.parallel cimport parallel, prange
 from libc.stdlib cimport abort, malloc, free
 from libc.stdio cimport *
 
-cdef unsigned long MAGIC_NUMBER = 14159265
-cdef unsigned long CURRENT_VERSION_WITH_FREQ = 215
-cdef unsigned long CURRENT_VERSION = 2048 + 215
+cdef unsigned int MAGIC_NUMBER = 14159265
+cdef unsigned int CURRENT_VERSION_WITH_FREQ = 215
+cdef unsigned int CURRENT_VERSION = 2048 + 215
+
+if sizeof(unsigned int) != 4:
+    print('size_t %i bytes' % sizeof(unsigned int))
+    print('size_t %i bytes' % sizeof(size_t))
+    raise ImportError('unsigned int needs to be 4 bytes not %i bytes' % sizeof(unsigned int))
+
 
 cdef bytes to_bytes(int_):
     return int_.to_bytes(4, 'little')
@@ -19,7 +25,7 @@ cdef int to_integer(byte_):
 #    cdef int int_ = (int) byte_
 #    return int_
 
-cdef void read_next_long(void *data, FILE *binary_file):
+cdef inline void read_next_long(void *data, FILE *binary_file) nogil:
     fread(data, 4, 1, binary_file) # little endian
 
 
@@ -28,31 +34,28 @@ cdef void read_next_long(void *data, FILE *binary_file):
 
 cdef extern from "stdio.h":
     #FILE * fopen ( const char * filename, const char * mode )
-    FILE *fopen(const char *, const char *)
+    FILE *fopen(const char *, const char *) nogil
     #int fclose ( FILE * stream )
-    int fclose(FILE *)
-    #ssize_t getline(char **lineptr, size_t *n, FILE *stream);
-    ssize_t getline(char **, size_t *, FILE *)
+    int fclose(FILE *) nogil
+    ##ssize_t getline(char **lineptr, size_t *n, FILE *stream);
+    #ssize_t getline(char **, size_t *, FILE *)
     #size_t fread ( void *ptr, size_t size, size_t count, FILE *stream );
-    size_t fread ( void *, size_t, size_t, FILE *)
+    size_t fread (void *, size_t, size_t, FILE *) nogil
 
 def learn_inplace(binary_file_paths, np.ndarray[double, ndim=2] weights,
                   double alpha, double beta1,
                   double beta2, double lambda_,
-                  np.ndarray[unsigned long, ndim=1] all_outcomes,
-                  unsigned long chunksize,
+                  np.ndarray[unsigned int, ndim=1] all_outcomes,
+                  unsigned int chunksize,
                   unsigned int number_of_threads):
 
-    cdef unsigned long n = weights.shape[0]
-    cdef unsigned long m = weights.shape[1]
-#    cdef double alpha = alpha
-    # cdef double beta1 = beta1
-    # cdef double beta2 = beta2
-    # cdef double lambda_ = lambda_
-    cdef unsigned long* all_outcomes_ptr = <unsigned long *> all_outcomes.data
-    cdef unsigned long length_all_outcomes = all_outcomes.shape[0]
+    cdef unsigned int n = weights.shape[0]
+    cdef unsigned int m = weights.shape[1]
+    cdef unsigned int* all_outcomes_ptr = <unsigned int *> all_outcomes.data
+    cdef unsigned int length_all_outcomes = all_outcomes.shape[0]
     cdef char* fname
-    cdef unsigned long start_val
+    cdef unsigned int start_val, end_val, ii
+    cdef unsigned int error = 4
 
   #  cdef String
     # weights muss contigousarray sein und mode=c, siehe:
@@ -63,17 +66,22 @@ def learn_inplace(binary_file_paths, np.ndarray[double, ndim=2] weights,
       filename_byte_string = binary_file_path.encode("UTF-8")
       fname = filename_byte_string
 
-#      with nogil, parallel():
-      for ii in range(int(length_all_outcomes / chunksize) + 1 ):
-        start_val = ii * chunksize
-        end_val = min(start_val + chunksize, length_all_outcomes)
-        if start_val == length_all_outcomes:
-          break
-        learn_inplace_ptr(fname, weights_ptr, n, m, alpha, beta1,
-                          beta2, lambda_, all_outcomes_ptr, start_val,
-                          end_val)
+      with nogil, parallel():
+        for ii in range((length_all_outcomes // chunksize) + 1 ):
+          start_val = ii * chunksize
+          end_val = min(start_val + chunksize, length_all_outcomes)
+          if start_val == length_all_outcomes:
+            break
+          error = learn_inplace_ptr(fname, weights_ptr, n, m, alpha, beta1,
+                            beta2, lambda_, all_outcomes_ptr, start_val,
+                            end_val)
 
-cdef int is_element_of(unsigned long elem, unsigned long* arr, unsigned long size):
+    if (error != 0):
+        raise IOError('binary files does not have proper format, error code %i' % error)
+
+
+cdef int is_element_of(unsigned int elem, unsigned int* arr, unsigned int size) nogil:
+    cdef unsigned int ii
     for ii in range(size):
       if arr[ii] == elem:
         return True
@@ -81,20 +89,20 @@ cdef int is_element_of(unsigned long elem, unsigned long* arr, unsigned long siz
 
 # ggf exception zurückgeben
 cdef int learn_inplace_ptr(char* binary_file_path, double* weights,
-                        unsigned long n, unsigned long m,
+                        unsigned int n, unsigned int m,
                         double alpha, double beta1,
                         double beta2, double lambda_,
-                        unsigned long* all_outcome_indices,
-                        unsigned long start,
-                        unsigned long end):
+                        unsigned int* all_outcome_indices,
+                        unsigned int start,
+                        unsigned int end) nogil:
 
 
-    cdef unsigned long number_of_events, number_of_cues, number_of_outcomes, frequency_counter
+    cdef unsigned int number_of_events, number_of_cues, number_of_outcomes, frequency_counter
     cdef double association_strength, update
 #    cdef np.ndarray[long] cue_indices, outcome_indices
-    cdef unsigned long magic_number, version, ii, jj
-    cdef unsigned long* cue_indices
-    cdef unsigned long* outcome_indices
+    cdef unsigned int magic_number, version, ii, jj, event
+    cdef unsigned int* cue_indices
+    cdef unsigned int* outcome_indices
     cdef int has_frequency # should be changed to an equivalent of boolean
 
     cdef FILE* cfile
@@ -110,7 +118,6 @@ cdef int learn_inplace_ptr(char* binary_file_path, double* weights,
         has_frequency = False
     else:
       weights[0] = version
-  #    weights[1] = CURRENT_VERSION
       return 2
 
     read_next_long(&number_of_events, binary_file)
@@ -120,13 +127,13 @@ cdef int learn_inplace_ptr(char* binary_file_path, double* weights,
 
         # Cues
         read_next_long(&number_of_cues, binary_file)
-        cue_indices = <unsigned long *> malloc(sizeof(unsigned long) * number_of_cues)
+        cue_indices = <unsigned int *> malloc(sizeof(unsigned int) * number_of_cues)
         for ii in range(number_of_cues):
           read_next_long(&cue_indices[ii], binary_file)
 
         # outcomes
         read_next_long(&number_of_outcomes, binary_file)
-        outcome_indices = <unsigned long *> malloc(sizeof(unsigned long) * number_of_outcomes)
+        outcome_indices = <unsigned int *> malloc(sizeof(unsigned int) * number_of_outcomes)
         for jj in range(number_of_outcomes):
           read_next_long(&outcome_indices[jj], binary_file)
 
@@ -139,11 +146,11 @@ cdef int learn_inplace_ptr(char* binary_file_path, double* weights,
             else:
               update = beta2 * (0.0 - association_strength)
             for ii in range(number_of_cues):
-              weights[0] = 1.0
+              #weights[0] = 1.0
+              weights[m * ii + jj] += alpha * update
               #weights[all_outcome_indices * ii ][cue_indices * jj] += alpha * update
               # weights[0] += alpha * update
               #pass
-               #               weights[m * ii + jj] += alpha * update
 
         free(cue_indices)
         free(outcome_indices)
