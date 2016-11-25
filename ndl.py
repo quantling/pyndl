fixes nogil inline code; compiles but gives wrong results half the time wring results

ndl.py
@@ -391,11 +391,9 @@ def binary_inplace_numpy_ndl_parrallel_thread(event_path, alpha, betas, lambda_,
 
 #    partlists_of_outcome_indices = slice_list(all_outcome_indices,sequence)
 
-
-
     ndl_parallel.learn_inplace(binary_files, weights, alpha,
                                 beta1, beta2, lambda_,
-                                np.array(all_outcome_indices, dtype=np.uint64),
+                                np.array(all_outcome_indices, dtype=np.uint32),
                                 sequence,
                                 number_of_threads)
 

tests/test_ndl.py
@@ -121,15 +121,16 @@ def test_compare_weights_numpy_inplace_parrallel_thread():
     shape = (len(outcome_map),len(cue_map))
     result_zeros = np.zeros(shape, dtype=np.float64, order='C')
 
-    #events = ndl.events(FILE_PATH, frequency=True)
-    #result_dict_ndl = ndl.dict_ndl_simple(events, ALPHA, BETAS,
-    #                                                  LAMBDA_, all_outcomes)
+    events = ndl.events(FILE_PATH, frequency=True)
+    result_dict_ndl = ndl.dict_ndl_simple(events, ALPHA, BETAS, LAMBDA_,
+                                          all_outcomes)
 
     assert len(result_zeros) == len(result_inplace_ndl)
+    assert len(result_dict_ndl) == len(result_inplace_ndl)
     #assert len(result_numpy_ndl[0]) == len(result_dict_ndl[0])
 
-    unequal = compare_arrays(FILE_PATH, result_inplace_ndl, result_zeros,
-                             is_np_arr2=True)
+    #unequal = compare_arrays(FILE_PATH, result_inplace_ndl, result_zeros, is_np_arr2=True)
+    unequal = compare_arrays(FILE_PATH, result_inplace_ndl, result_dict_ndl, is_np_arr2=False)
     #print(unequal)
     print('%.2f ratio unequal' % (len(unequal) / (len(outcome_map) * len(cue_map))))
     assert len(unequal) == 0