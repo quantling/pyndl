916ff13abbc36cb1cef799de1654b1a097bd5f7f
Tino Sering <konstantin.sering@uni-tuebingen.de>
2017-02-17 14:45:47 +0100

    improves README.rst; adds numpydoc as an extension for sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,7 +35,9 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.doctest',
     'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
-    'sphinx.ext.viewcode']
+    'sphinx.ext.viewcode',
+    'numpydoc',
+    ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
