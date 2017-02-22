1b5cefcfcee0e01f6dd85aa91039ddc4ac54f6d4
sumny <lennart.sch@web.de>
2017-02-22 17:43:55 +0100

    excluded numpydoc until fixed

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -36,7 +36,7 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
-    'numpydoc',
+#    'numpydoc',
     ]
 
 # Add any paths that contain templates here, relative to this directory.
