47b31955f395ced432886b82d2fe1ac7d52e217b
Trybnetic <marc.weitz@onlinehome.de>
2017-03-08 12:55:40 +0100

    adds metadata to __init__.py

---
+++ pyndl/__init__.py
@@ -0,0 +1,33 @@
+__author__ = ('David-Elias Künstle, Lennard Schneider, '
+              'Konstantin Sering, Marc Weitz')
+__author_email__ = 'konstantin.sering@uni-tuebingen.de'
+__version__ = '0.1dev'
+__license__ = 'MIT'
+__description__ = ('Naive discriminative learning implements learning and '
+                   'classification models based on the Rescorla-Wagner equations.')
+__classifiers__ = [
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+    ]
+
+
+__doc__ = """
+:abstract: %s
+:version: %s
+:author: %s
+:contact: %s
+:date: 2012-02-08
+:copyright: %s
+""" % (__description__, __version__, __author__, __author_email__, __license__)
