71b8371761684e08a3102ce6d588b467fa47e0e0
Trybnetic <marc.weitz@onlinehome.de>
2017-12-23 02:21:23 +0100

    exports conda path

--- .travis/install.sh
+++ .travis/install.sh
@@ -3,6 +3,8 @@
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
   wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh;
   bash miniconda.sh -b -p $HOME/miniconda;
+  export PATH="$HOME/miniconda/bin:$PATH"
+  conda info -a
   if [[ $TOXENV == "py35" ]]; then
     conda install python=3.5;
   elif [[$TOXENV == "py36" ]]; then
