c51a7d90371f2deaa466b1a57f72d87b61d5f976
Trybnetic <marc.weitz@onlinehome.de>
2017-12-23 02:27:39 +0100

    configures conda

--- .travis/install.sh
+++ .travis/install.sh
@@ -4,12 +4,21 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
   wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh;
   bash miniconda.sh -b -p $HOME/miniconda;
   export PATH="$HOME/miniconda/bin:$PATH"
+  hash -r
+  conda config --set always_yes yes --set changeps1 no
+  conda update -q conda
+  conda config --add channels conda-forge
   conda info -a
-  if [[ $TOXENV == "py35" ]]; then
-    conda install python=3.5;
-  elif [[$TOXENV == "py36" ]]; then
-    conda install python=3.6;
-  fi
+  case "${TOXENV}" in
+        py35)
+            conda install python=3.5;
+            ;;
+        py36)
+            conda install python=3.6;
+            ;;
+  esac
+  conda info
+  which python
 else
     pip install tox-travis;
 fi
