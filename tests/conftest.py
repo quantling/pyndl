0ed49901c4ad95452e950bfbc20bab3d1bb0f3f0
Marc Weitz <marc.weitz@onlinehome.de>
2020-01-13 17:09:17 +0100

    Adds some maintenance changes (#179)
    * whitelists rm from path
    
    * improves code style
    
    * increases version

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,6 +9,7 @@ def pytest_addoption(parser):
     parser.addoption("--runslow", action="store_true",
                      help="run slow tests")
 
+
 def pytest_runtest_setup(item):
     if 'runslow' in item.keywords and not item.config.getoption("--runslow"):
         pytest.skip("need --runslow option to run this test")
